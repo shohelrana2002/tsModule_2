ae33cd700f9e4b749f344f44eabb3f36f3006f08
shohelrana2002 <rana2021tang@gmail.com>
2024-11-08 12:07:37 +0600

    Add to generic type Array <Number ,boolean string> add normal

---
+++ src/2.3.ts
@@ -0,0 +1,11 @@
+{
+  // generic type
+  //   const rollNumber: number[] = [10, 20, 20, 2365, 6, 6, 656, 256];
+  const rollNumber: Array<number> = [10, 20, 20, 2365, 6, 6, 656, 256];
+
+  //   const mentors: string[] = ["Mr.s ", "Mr.y", "mr.z"];
+  const mentors: Array<string> = ["Mr.s ", "Mr.y", "mr.z"];
+
+  //   const bollArray: boolean[] = [true, false, true];
+  const bollArray: Array<boolean> = [true, false, true];
+}
